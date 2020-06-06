0e073e36a32381c445d77700c6e663ae4d7c6f91
Alyona Uskova <aluskova01@yandex.ru>
2020-06-06 16:35:49 +0300

    some files instead one

--- prog.py
+++ prog.py
@@ -1,34 +1,32 @@
 import pandas as pd
-import xlrd
 import datetime
+import xlrd
 import xlwt
 import openpyxl
 #D:\19ПИ-3.xlsx - это путь к файлу
 class STUDENT(object):
-    def __init__(self, name='', group='', points=[], midpoint=int, respoint=int):
+    def __init__(self, name='', group='', results=[]):
         self.name = name
         self.group = group
-        self.points = points
-        self.midpoint = midpoint
-        self.respoint = respoint
+        self.results = results
 
     def show_student(self):
         discription = (str(self.name) + " " + str(self.group) + " middle point is " + str(
             self.midpoint) + " result point is " + str(self.respoint))
         print(discription)
-def Сurrent_student_grades(file_name : str):
-    if type(file_name) is str:
-        students = []
-        print('Enter filename(number of group): ')
-        filename = str(input())
-        group = filename
+def Current_student_grades(*file_names):
+    students = []
+    for file_num in range(int(len(file_names))):
+        file_name = file_names[file_num]
         rb = xlrd.open_workbook(file_name, formatting_info=False)
         if rb == 0:
-            print('Open error')
+            print(str("Error of opening: " + str(file_name)))
+            continue
         sheet = rb.sheet_by_index(0)            #выбираем активный лист
-        row = []
         row_number = sheet.nrows
         col_number = sheet.ncols
+        group = sheet.cell(rowx=1, colx=sheet.ncols).value
+        subject = sheet.cell(rowx=2, colx=sheet.ncols).value
         i = 2
         kolvo = 0
         while sheet.cell(rowx=0, colx=i).value != 'Средний балл' and sheet.cell(rowx=1, colx=i).value != 'Middle point':
@@ -40,24 +38,41 @@ def Сurrent_student_grades(file_name : str):
                 array = [0]*kolvo
                 j = 1
                 k = 0
-                while sheet.cell(rowx=0, colx=j+1).value != 'Средний балл' and sheet.cell(rowx=1, colx=j).value != 'Middle point':
+                flag = 0
+                dict_ = {}
+                while sheet.cell(rowx=0, colx=j + 1).value != 'Средний балл' and sheet.cell(rowx=1,
+                                                                                            colx=j).value != 'Middle point':
+                    cell = sheet.cell(rowx=0, colx=j + 1)
                     j += 1
-                    array[k] = str(sheet.cell(rowx=i, colx=j).value)
-                    k+=1
-                Student = STUDENT(sheet.cell(rowx=i, colx=1).value, group, array, sheet.cell(rowx=i, colx=kolvo+2).value,
-                                  sheet.cell(rowx=i, colx=1+kolvo+2).value)
-                students.append(Student)
-                #Student.show_student()
-                array.clear()
+                    if cell.ctype == 3:
+                        year, month, day, hour, minute, second = xlrd.xldate_as_tuple(cell.value, rb.datemode)
+                        value = datetime.datetime(year, day, month)
+                    else:
+                        value = cell.value
+                    mark = str(sheet.cell(rowx=i, colx=j).value)
+                    dict_.update({str(value): mark})
+                dict_.update({sheet.cell(rowx=0, colx=kolvo + 2).value: sheet.cell(rowx=i, colx=kolvo + 2).value})
+                dict_.update({sheet.cell(rowx=0, colx=kolvo + 3).value: sheet.cell(rowx=i, colx=kolvo + 3).value})
+                for m in students:
+                    if students[m].name == sheet.cell(rowx=i, colx=1).value:
+                        flag = 1
+                        students[m].results.append(subject)
+                        students[m].results.append(dict_)
+                if flag == 0:
+                    results = []
+                    results.append(subject)
+                    results.append(dict_)
+                    Student = STUDENT(sheet.cell(rowx=i, colx=1).value, group, results)
+                    students.append(Student)
+                    results.clear()
         else:
-                return -1
+            print('Empty file' + file_name)
+            continue
         return students
-    else:
-        return -1
-print('Enter the way to your file(with name of this file): ')
-way_ = str(input())
-arr = []
-arr = Сurrent_student_grades(way_)
-i = 0
-for i in range (len(arr)):
-    arr[i].show_student()
+#print('Enter the way to your file(with name of this file): ')
+#way_ = str(input())
+#arr = []
+#arr = Current_student_grades(way_)
+#i = 0
+#for i in range (len(arr)):
+ #   arr[i].show_student()
